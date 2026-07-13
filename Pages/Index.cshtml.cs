2a86f258ab4a87b97b69a19022ac8e73858e9325
Dina <dina.smail@list.ru>
2026-07-13 23:29:06 +0300

    Add contact form with POST handler saving to database

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CleaningSite.Data;
+using CleaningSite.Models;
 
 namespace CleaningSite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly AppDbContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, AppDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
+
+        [BindProperty]
+        public ContactRequest Request { get; set; } = new();
+
 
         public void OnGet()
         {
+        }
 
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.ContactRequests.Add(Request);
+            _db.SaveChanges();
+
+            TempData["Success"] = true;
+            return RedirectToPage();
         }
     }
 }
