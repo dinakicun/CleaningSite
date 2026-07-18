2660e374671f7c641bf7fef04af60c0056a10c0c
Dina <dina.smail@list.ru>
2026-07-18 23:43:56 +0300

    Style contact section, remove Bootstrap container for full-width layout

--- wwwroot/js/contact-form.js
+++ wwwroot/js/contact-form.js
@@ -11,8 +11,7 @@ form.addEventListener('submit', async function (event) {
     if (response.ok) {
         contactSection.innerHTML = '<div class="contact__success">Спасибо! Мы приняли вашу заявку и перезвоним в ближайшее время.</div>';
     } else {
-        const errorText = await response.text();
-        alert(errorText);
+      
     }
 });
 
